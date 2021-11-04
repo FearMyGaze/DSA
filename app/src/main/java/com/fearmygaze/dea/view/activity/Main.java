27c2eacd26d5c687c2d54726a1e7748daf527091
Giorgos Papoulias <FearMyGaze@outlook.com>
2021-11-04 16:22:23 +0200

    convert to lamda

--- app/src/main/java/com/fearmygaze/dea/view/activity/Main.java
+++ app/src/main/java/com/fearmygaze/dea/view/activity/Main.java
@@ -2,40 +2,38 @@ package com.fearmygaze.dea.view.activity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.fearmygaze.dea.R;
+import com.fearmygaze.dea.custom.MyToast.CustomToast;
 import com.fearmygaze.dea.view.fragment.Home;
 import com.fearmygaze.dea.view.fragment.Notifications;
 import com.fearmygaze.dea.view.fragment.PatientFiles;
 import com.fearmygaze.dea.view.fragment.Profile;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class Main extends AppCompatActivity {
 
     public Fragment home, patientFiles, notifications, profile;
+    public CustomToast customToast;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        /*
-        * TODO: CHANGE THE BottomNavigationView Like Monsta
-        * */
 
         home = new Home();
         profile = new Profile();
         patientFiles = new PatientFiles();
         notifications = new Notifications();
+        customToast = new CustomToast(Main.this);
 
         replaceFragment(home);
 
@@ -52,26 +50,22 @@ public class Main extends AppCompatActivity {
         *}
         * bottomNavigation.removeBadge(menuItemId)
         * */
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.itemHome:
-                        replaceFragment(home);
-                        return true;
-                    case R.id.itemNotifications:
-                        replaceFragment(notifications);
-                        return true;
-                    case R.id.itemPatientFiles:
-                        replaceFragment(patientFiles);
-                        return true;
-                    case R.id.itemProfile:
-                        replaceFragment(profile);
-                        return true;
-                    default:
-                        return false;
-                }
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.itemHome:
+                    replaceFragment(home);
+                    return true;
+                case R.id.itemNotifications:
+                    replaceFragment(notifications);
+                    return true;
+                case R.id.itemPatientFiles:
+                    replaceFragment(patientFiles);
+                    return true;
+                case R.id.itemProfile:
+                    replaceFragment(profile);
+                    return true;
+                default:
+                    return false;
             }
         });
 
